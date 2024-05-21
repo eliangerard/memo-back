71585e985e3dce8fae2639dac127262873012801
eliangerard <eliangerardgg@gmail.com>
2024-05-20 23:55:18 -0600

    AAA

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const { Client } = require('@threadsjs/threads.js');
-const OpenAI = require('openai');
 const Post = require('./Post');
-
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 mongoose.connect(process.env.MONGO_URI) .then(() => { console.log('Connected to MongoDB'); }) .catch((error) => { console.error('Failed to connect to MongoDB:', error); });
 
@@ -14,26 +10,6 @@ const app = express();
 app.use(cors());
 
 app.use(express.json({ limit: '100mb' }));
-
-const client = new Client();
-
-// client.login(process.env.IG_USER, process.env.IG_PASS).then(() => {
-//     console.log('Logged in');
-// });
-
-const getEmotion = async (caption) => {
-    const emotions = ['happy', 'sad', 'angry', 'fear', 'disgust', 'surprise'];
-    const prompt = `WHAT OF THE NEXT EMOTIONS (${emotions.join(',')}) WOULD A PERSON HAVE TO ENJOY A FACEBOOK POST WITH THIS DESCRIPTION: ${caption} | ANSWER ONLY THE EMOTION AND NOTHING ELSE NO MATTER WHAT, IF MORE THAN ONE EMOTION IS REQUIRED, SEPARATE THEM WITH A COMMA.`;
-
-    const chatCompletion = await openai.chat.completions.create({
-        messages: [{ role: 'user', content: prompt }],
-        model: 'gpt-3.5-turbo',
-    });
-
-    console.log(chatCompletion);
-    return chatCompletion.choices[0].message.content.toLowerCase();
-
-}
 
 app.get('/feeds', async (req, res) => {
 
